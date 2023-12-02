f8610c5f437eb7cc4849ff36d0c592ec658de9ca
isabellirosa <isabelli.ifc@gmail.com>
2023-12-01 21:13:58 -0300

    tentando arrumar

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
 // Script do cardápio carrosel
+for (let index = 1; index < 6; index++) {
 var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-var cardWidth = $(".produto").width();
+var cardWidth = $(`.produto${index}`).width();
 var scrollPosition = 0;
 
-$(".carousel-control-next").on("click", function () {
+$(`.carousel-control-next`).on("click", function () {
     console.log(carouselWidth,cardWidth,scrollPosition);
-    if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+    if (scrollPosition < (carouselWidth - cardWidth * 4)&& button.classList.contains(`${index}`)) { //check if you can go any further
         scrollPosition += cardWidth;  //update scroll position
         $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
     }
 });
 
-$(".carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
+$(`.carousel-control-prev`).on("click", function () {
+    if (scrollPosition > 0 && div.classList.contains(`${index}`)) {
         scrollPosition -= cardWidth;
         $(".carousel-inner").animate(
             { scrollLeft: scrollPosition },
@@ -36,3 +37,4 @@ var carousel = new bootstrap.Carousel(multipleCardCarousel, {
 interval: false,
 wrap: false,
 });
+}
