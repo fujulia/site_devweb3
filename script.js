ff516ca75350e90067539f690480768f9a5cbc0c
fujulia <juliafu713@gmail.com>
2023-12-03 22:58:22 -0300

    feito

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var cardWidth = $(".produto").width();
 var scrollPosition = 0;
 
 $(".carousel-control-next").on("click", function () {
-    console.log(carouselWidth,cardWidth,scrollPosition);
+    console.log(carouselWidth, cardWidth, scrollPosition);
     if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
         scrollPosition += cardWidth;  //update scroll position
         $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
@@ -22,17 +22,34 @@ $(".carousel-control-prev").on("click", function () {
     }
 });
 var multipleCardCarousel = document.querySelector(
-"#carouselExampleControls"
+    "#carouselExampleControls"
 );
 if (window.matchMedia("(min-width: 768px)").matches) {
-//rest of the code
-var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-interval: false
-});
+    //rest of the code
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+        interval: false
+    });
 } else {
-$(multipleCardCarousel).addClass("slide");
+    $(multipleCardCarousel).addClass("slide");
 }
 var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-interval: false,
-wrap: false,
+    interval: false,
+    wrap: false,
 });
+
+function openMenu(){
+    if(window.innerWidth <= 950){
+        const menuburger = document.getElementById('menuH');
+        const navlist = document.querySelector('.menu');
+
+        if(menuburger.classList.value.indexOf("bx-x") > -1){
+            navlist.classList.remove("openMenu");
+            menuburger.classList.remove("bx-x");
+            menuburger.classList.add("bx-menu");
+        }else{
+            navlist.classList.add("openMenu");
+            menuburger.classList.remove("bx-menu");
+            menuburger.classList.add("bx-x");
+        }
+    }
+};
